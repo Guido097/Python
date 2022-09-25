22178c19da04a386b4621ce6d4fd9fdecdedf793
Guido097 <112497761+Guido097@users.noreply.github.com>
2022-09-25 12:18:10 -0300

    ''

--- Dia 3/1_Metodo_Index.py
+++ Dia 3/1_Metodo_Index.py
@@ -2,3 +2,35 @@ mi_texto = 'Esta es una prueba'
 resultado = mi_texto[0]
 
 print(resultado)
+
+mi_texto = 'Esta es una prueba'
+resultado = mi_texto.index('n')
+
+print(resultado)
+
+mi_texto = 'Esta es una prueba'
+resultado = mi_texto.index('prueba')
+
+print(resultado)
+
+mi_texto = 'Esta es una prueba'
+resultado = mi_texto.index('a')
+
+print(resultado)
+
+mi_texto = 'Esta es una prueba'
+resultado = mi_texto.index('a',5)
+
+print(resultado)
+
+mi_texto = 'Esta es una prueba'
+resultado = mi_texto.index('a',5,11)
+
+print(resultado)
+
+#Metodo de derecha a izquierda con 'rindex'
+
+mi_texto = 'Esta es una prueba'
+resultado = mi_texto.rindex('a')
+
+print(resultado)
