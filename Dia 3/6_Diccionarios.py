84164fdeae93035833067131e287aeb12c6a907f
Guido097 <112497761+Guido097@users.noreply.github.com>
2022-10-13 17:54:05 -0300

    Another day

---
+++ Dia 3/6_Diccionarios.py
@@ -0,0 +1,3 @@
+#Un diccionario es una colección de pares y cada uno de esos pares tiene una clave y un valor asociado a esa clave
+#Ej. mi_dic = {'c1':'valor1','c2':'valor2','c3':'valor3'}
+#No importa el orden y se suele usar para ubicar un valor sin saber donde esta ubicado
